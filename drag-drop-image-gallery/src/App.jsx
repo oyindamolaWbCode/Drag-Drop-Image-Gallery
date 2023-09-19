7e670322f56252e17a7907c93145590ada4fdeb1
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-09-19 15:36:11 +0200

    environment setup

---
+++ drag-drop-image-gallery/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='The-Homepage'>
+        <h1>Drag and Drop Page</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
